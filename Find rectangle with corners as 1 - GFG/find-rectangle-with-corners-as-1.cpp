97dc1c3d0ed561a0903babac8701942808084f1e
Sahilkumar Katiriya <59791131+xposecode@users.noreply.github.com>
2022-02-16 09:53:42 +0530

    Added solution - LeetHub

---
+++ Find rectangle with corners as 1 - GFG/find-rectangle-with-corners-as-1.cpp
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+#include <unordered_map>
+#include <unordered_set>
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    bool ValidCorner(const vector<vector<int> >& matrix)
+    {
+        // Your code goes here
+        for(int i=0;i<matrix.size();i++){
+            for(int j=i+1;j<matrix.size();j++){
+                int count=0;
+                for(int k=0;k<matrix[i].size();k++)
+                    if(matrix[i][k] && matrix[j][k])    count++;
+                if(count>1) return true;
+            }
+        }
+        return false;
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int row, col;
+        cin>> row>> col;
+
+        vector<vector<int> > matrix(row);
+            
+        for(int i=0; i<row; i++)
+        {
+            matrix[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        Solution ob;
+        if (ob.ValidCorner(matrix)) 
+            cout << "Yes\n"; 
+        else
+            cout << "No\n"; 
+    }
+
+    return 0;
+} 
+
+
+  // } Driver Code Ends
