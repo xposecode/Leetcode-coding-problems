bc20e46490914c8bde03cc412358022f9e0a979d
Sahilkumar Katiriya <59791131+xposecode@users.noreply.github.com>
2022-02-21 10:51:51 +0530

    Time: 30 ms (41.73%), Space: 19.5 MB (88.96%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+            
+      sort(nums.begin(),nums.end()); // sort the vector
+        return nums[nums.size()/2];     // middle element will be the most frequent Element
+    }
+};
