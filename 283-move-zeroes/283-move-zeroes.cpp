ac59c677e007c7e1b8eb8d1db5a75108e3bedbae
Sahilkumar Katiriya <59791131+xposecode@users.noreply.github.com>
2022-03-19 13:13:28 +0530

    Time: 37 ms (42.58%), Space: 19.3 MB (58.35%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -3,7 +3,7 @@ public:
     void moveZeroes(vector<int>& arr) {
         
             
-            int i=0,j=0,n = arr.size();
+            int i=0,j=0,n = arr.size();      
             while(i<n)
             {
                     if(arr[i]!=0)
@@ -18,3 +18,16 @@ public:
             }
     }
 };
+
+/* Let's dry run using arr=[0,1,0,3,12]
+
+i: 1 2 3 4
+j: 0 1 1 2
+
+1,0,0,3,12
+
+1,3,0,0,12
+
+1,3,12,0,0
+
+*/
