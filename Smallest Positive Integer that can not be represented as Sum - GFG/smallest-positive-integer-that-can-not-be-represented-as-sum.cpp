0e50a40cf00713a7d857a62bf04660d443ff5f73
Sahilkumar Katiriya <59791131+xposecode@users.noreply.github.com>
2022-03-23 10:42:44 +0530

    Added solution - LeetHub

--- Smallest Positive Integer that can not be represented as Sum - GFG/smallest-positive-integer-that-can-not-be-represented-as-sum.cpp
+++ Smallest Positive Integer that can not be represented as Sum - GFG/smallest-positive-integer-that-can-not-be-represented-as-sum.cpp
@@ -14,18 +14,14 @@ public:
     long long smallestpositive(vector<long long> array, int n)
     { 
         // code here 
-        // int arr[n];
         sort(array.begin(),array.end());  // 1 3 6 10 11 15
-        long long sum=1;
+        long long sum=1;            
         for(auto i : array)
         {
-            // sum =sum + arr[i];
             if(sum>=i)
             {
                 sum+=i;
             }
-            
-            // arr[i]=sum;
         }
         return sum;
           
