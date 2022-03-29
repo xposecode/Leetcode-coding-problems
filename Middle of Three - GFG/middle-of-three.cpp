7a21e73fca2adf271fd4a1b044cb2214484ad506
Sahilkumar Katiriya <59791131+xposecode@users.noreply.github.com>
2022-03-29 08:37:15 +0530

    Added solution - LeetHub

---
+++ Middle of Three - GFG/middle-of-three.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int middle(int A, int B, int C){
+        //code here//Position this line where user code will be pasted.
+        int sum=A+B+C;
+        
+        int maxi= max({A,B,C});
+        int mini= min({A,B,C});
+        
+        return sum-(maxi+mini);
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A,B,C;
+        cin>>A>>B>>C;
+        Solution ob;
+        cout<<ob.middle(A,B,C) <<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
