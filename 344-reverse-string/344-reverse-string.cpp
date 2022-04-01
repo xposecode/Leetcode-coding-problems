512a66b8261ee8fce3a428ac7838b28cfdc725e3
Sahilkumar Katiriya <59791131+xposecode@users.noreply.github.com>
2022-04-01 12:03:45 +0530

    Time: 33 ms (42.38%), Space: 23.3 MB (41.72%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -4,15 +4,15 @@ public:
         
             //1st approach
             int n= s.size();
-            int i=0,end=n-1;
-            while(i<end)
-            {
-                    swap(s[i],s[end]);
-                    i++;
-                    end--;
-            }
+            // int i=0,end=n-1;
+            // while(i<end)
+            // {
+            //         swap(s[i],s[end]);
+            //         i++;
+            //         end--;
+            // }
             //2nd approach
-            // reverse(s.begin(),s.end());
+            reverse(s.begin(),s.end());
             
     }
         
