89df32f31c7d84549ad0fc80b83fa07618b46365
Sahilkumar Katiriya <59791131+xposecode@users.noreply.github.com>
2022-04-21 10:28:11 +0530

    Added solution - LeetHub

---
+++ Reverse digits - GFG/reverse-digits.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+
+class Solution:
+	def reverse_digit(self, n):
+		# Code here
+        return int(str(n)[::-1])
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		ob = Solution();
+		ans = ob.reverse_digit(n)
+		print(ans)
+# } Driver Code Ends
