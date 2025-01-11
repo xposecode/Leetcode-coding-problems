dfd36e78ae865ad2635e2ed1b335b23fec9a9ca3
Sahilkumar Katiriya <59791131+xposecode@users.noreply.github.com>
2025-01-11 11:49:33 +0530

    Time: 0 ms (100%), Space: 15 MB (51.75%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -1,21 +1,14 @@
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
     bool canConstruct(string s, int k) {
         if (s.length() < k) return false;
 
-        unordered_map<char, int> charCount;
+        int bitmask = 0;
         for (char c : s) {
-            charCount[c]++;
+            bitmask ^= (1 << (c - 'a'));
         }
 
-        int oddCount = 0;
-        for (auto& entry : charCount) {
-            if (entry.second % 2 != 0) oddCount++;
-        }
-
+        int oddCount = __builtin_popcount(bitmask);
         return oddCount <= k;
     }
 };
