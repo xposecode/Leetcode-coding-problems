73cecdb5bcc6631936e5188a50ad7b06679957c6
Sahilkumar Katiriya <59791131+xposecode@users.noreply.github.com>
2025-01-11 11:29:12 +0530

    Time: 2 ms (75.52%), Space: 15 MB (33.92%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -3,19 +3,12 @@ public:
     bool canConstruct(string s, int k) {
         if (s.length() < k) return false;
 
-        sort(s.begin(), s.end());
-        int oddCount = 0;
-
-        for (int i = 0; i < s.length(); ) {
-            char current = s[i];
-            int count = 0;
-            while (i < s.length() && s[i] == current) {
-                count++;
-                i++;
-            }
-            if (count % 2 != 0) oddCount++;
+        int bitmask = 0;
+        for (char c : s) {
+            bitmask ^= (1 << (c - 'a'));
         }
 
+        int oddCount = __builtin_popcount(bitmask);
         return oddCount <= k;
     }
 };
