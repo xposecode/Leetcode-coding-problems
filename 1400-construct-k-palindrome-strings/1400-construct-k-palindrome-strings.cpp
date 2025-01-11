f84ec234ae34b5ae3c689d65c3679c64d829440c
Sahilkumar Katiriya <59791131+xposecode@users.noreply.github.com>
2025-01-11 11:28:26 +0530

    Time: 13 ms (31.12%), Space: 15.2 MB (21.68%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -1,30 +1,31 @@
 class Solution
 {
-    public:
-        bool canConstruct(string s, int k)
+public:
+    bool canConstruct(string s, int k)
+    {
+        // If k is greater than the length of the string, we can't create k palindromes
+        if (k > s.size())
+            return false;
+
+        // Count the frequency of each character in the string
+        unordered_map<char, int> charCount;
+        for (char c : s)
         {
-
-            int len = s.length();
-
-            if (len < k)
-            {
-                return false;
-            }
-
-            int count = 0;
-            map<char, int> freq;
-            for (int i = 0; i < len; ++i)
-            {
-                freq[s[i]]++;
-            }
-            for (auto it = freq.begin(); it != freq.end(); ++it)
-            {
-                if (it->second % 2 != 0)
-                {
-                    count++;
-                }
-            }
-        
-    return count <= k;
+            charCount[c]++;
         }
+
+        // Count the number of characters with odd frequencies
+        int oddCount = 0;
+        for (const auto& pair : charCount)
+        {
+            if (pair.second % 2 != 0)
+            {
+                oddCount++;
+            }
+        }
+
+        // To form k palindromes, we need at least `oddCount` palindromes (one for each odd character count)
+        // If `oddCount` is less than or equal to k, we can construct k palindromes
+        return oddCount <= k;
+    }
 };
